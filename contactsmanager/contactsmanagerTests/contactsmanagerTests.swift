c48c2523f6092c55f030f3f202d481d9bcd3c243
Tan Chong Pui <cpt@outlook.my>
2019-08-07 21:43:47 +0800

    project creation

---
+++ contactsmanager/contactsmanagerTests/contactsmanagerTests.swift
@@ -0,0 +1,34 @@
+//
+//  contactsmanagerTests.swift
+//  contactsmanagerTests
+//
+//  Created by Dev on 07/08/2019.
+//  Copyright © 2019 Dev. All rights reserved.
+//
+
+import XCTest
+@testable import contactsmanager
+
+class contactsmanagerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
